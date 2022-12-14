977c39872a38b1eabd5d608be726aa471f116dc7
lexpk <alexander.pluska@gmail.com>
2022-12-14 22:26:59 +0100

    initial commit

---
+++ python/learned_equational_rewriting/__init__.py
@@ -0,0 +1,6 @@
+from .learned_equational_rewriting import *
+
+
+__doc__ = learned_equational_rewriting.__doc__
+if hasattr(learned_equational_rewriting, "__all__"):
+	__all__ = learned_equational_rewriting.__all__
